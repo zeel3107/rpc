9637a5cdb1bd88ec6bdecda6830dc08a5edd412e
Zeel Patel <zeelpatel@Zeels-MacBook-Pro-2.local>
2024-11-11 23:49:44 -0500

    2nd

--- rpc.py
+++ rpc.py
@@ -1,5 +1,3 @@
 def main():
-    print("Welcome to Rock, Paper, Scissors!")
-
-if __name__ == "_main_":
-    main()
+      print("Welcome to Rock Paper Scissors!")
+      print("Rules: Rock beats scissors, scissors beats paper, paper beats rock.")
